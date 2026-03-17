8e7de06486b3b1aa70715308137955634cabced2
saqibsumra <saqib.sumra-5@gmail.com>
2026-03-17 21:03:44 +0100

    add missing public exports

--- src/spotipy/__init__.py
+++ src/spotipy/__init__.py
@@ -1,26 +1,34 @@
 """
 SpotiPy: Solar Spot Analysis Tool
----------------------------------
-A library for downloading, processing, and tracking solar features.
+----------------------------------
+A library for downloading, processing, and analysing solar features
+from SDO/HMI and SDO/AIA data.
 """
 
-# Import the modules so users can access them directly
-# e.g., spotipy.download_data()
-
-from .downloading import download_data
+from .downloading import download_series, load_file_list, save_file_list, ask_yn
 from .aligning import align_images
-from .limbdarkening_removal import remove_limb_darkening
+from .limbdarkening_removal import remove_limb_darkening, get_header_geometry
 from .segmentation import get_masks, get_aia_masks
 from .tracking import track_spots, refine_centering
 from .gui_tools import get_manual_coordinates
 
 __all__ = [
-    "download_data",
-    "aligning",
+    # Downloading
+    "download_series",
+    "load_file_list",
+    "save_file_list",
+    "ask_yn",
+    # Aligning
+    "align_images",
+    # Limb Darkening
     "remove_limb_darkening",
+    "get_header_geometry",
+    # Segmentation
     "get_masks",
     "get_aia_masks",
+    # Tracking
     "track_spots",
     "refine_centering",
-    "get_manual_coordinates"
+    # GUI
+    "get_manual_coordinates",
 ]
